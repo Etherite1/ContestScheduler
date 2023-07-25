bfdc41cea401f6c3bb194a02b1b1e30df2819b23
Humphrey Xu <29512438+Etherite1@users.noreply.github.com>
2023-07-25 12:39:14 -0500

    added logos, forward sorting by column

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,15 @@ import GetContests from './GetContests.jsx'
 
 function Contest({site, name, time, countdown}) // TODO: link
 {
+    function getLogo(site)
+    {
+        if(site == "CC") return "logos/cc.png";
+        else if(site == "LC") return "logos/lc.png";
+        else if(site == "CF") return "logos/cf.png";
+    }
     return (
         <tr>
-            <td>{site}</td>
+            <img src = {getLogo(site)} height = "50px" width = "auto" />
             <td>{name}</td>
             <td>{time}</td>
             <td>{countdown}</td>
@@ -17,23 +23,33 @@ function Contest({site, name, time, countdown}) // TODO: link
 
 function ContestTable({displayedSites}) 
 {
-    var contests = GetContests();
+    const [contests, setContests] = useState(GetContests());
 
-    const contestComponents = contests.map((contest) => {
+    var contestComponents = contests.map((contest) => {
         if(!displayedSites.get(contest[0])) return;
         return (
             <Contest key = {contest[0]} site = {contest[0]} name = {contest[1]} time = {contest[2]} countdown = {contest[3]}/>
         );
     });
+    
+    function applySorting(col) {
+        var nextContests = contests.slice();
+        nextContests.sort(function(x, y) {
+            if(x[col] < y[col]) return -1;
+            else if(x[col] === y[col]) return 0;
+            else return 1;
+        });
+        setContests(nextContests);
+    }
 
     return (
         <table className = "table table-hover mb-0">
             <thead>
                 <tr>
-                    <th>Site</th>
-                    <th style = {{width: "500px"}}>Name</th>
-                    <th>Time</th>
-                    <th>Countdown</th>
+                    <th onClick = {() => applySorting(0)}>Site</th>
+                    <th onClick = {() => applySorting(1)} style = {{width: "500px"}}>Name</th>
+                    <th onClick = {() => applySorting(2)} >Time</th>
+                    <th onClick = {() => applySorting(3)} >Countdown</th>
                 </tr>
             </thead>
             <tbody>
