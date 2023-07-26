3205e11d2e75f9272305a447d8d1d808a8de6578
Humphrey Xu <29512438+Etherite1@users.noreply.github.com>
2023-07-25 21:08:43 -0500

    unblocked CORS

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from flask import Flask
+from flask_cors import CORS
 import json
 
 from src import leetcode_scraper
 from src import codeforces_scraper
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def scrape():
     # leetcode_scraper.scrape()
-    return json.dumps(codeforces_scraper.scrape());
+    return json.dumps(codeforces_scraper.scrape())
--- src/main.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-import leetcode_scraper
-import codeforces_scraper
-
-if __name__ == '__main__':
-    # scrape_open_html()
-    leetcode_scraper.scrape()
-    codeforces_scraper.scrape();
