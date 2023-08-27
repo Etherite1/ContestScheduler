797c11fcdebf0993dba97ae487ea85a1d9e34ac0
Caleb Xu <caleb.xu.77@gmail.com>
2023-08-27 18:34:14 -0500

    yet another random ass commit

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 from __future__ import print_function
-import sys
-print(sys.path)
-
-
 from flask import Flask, jsonify
-
+from flask_cors import CORS
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-from flask_cors import CORS
+
 
 from datetime import datetime
 import pytz
